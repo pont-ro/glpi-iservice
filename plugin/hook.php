055dbbee0bd27e7db2cab9420ac6ae081b92c453
hupu <hlavathy.peter@gmail.com>
2023-05-19 13:16:13 +0300

    Added the plugin with asset overwriting

---
+++ plugin/hook.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Install all necessary elements for the plugin
+ * @param array $args Arguments passed from CLI
+ * @return boolean
+ */
+function plugin_iservice_install(array $args = []): bool
+{
+//    $migration = new Migration(plugin_version_iservice()['version']);
+    require_once(__DIR__ . '/install/install.php');
+    $install = new PluginIserviceInstall();
+//    if (!$install->isPluginInstalled() || ($args['force-install'] ?? false) === true) {
+        return $install->install();
+//    }
+
+//    return $install->upgrade($migration, $args);
+}
+
+function plugin_iservice_uninstall(): void
+{
+    require_once(__DIR__ . '/install/install.php');
+    $install = new PluginIserviceInstall();
+    $install->uninstall();
+}
