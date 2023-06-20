86b4f23055d1ab9eaa43cb7e5511f39a03494257
Zsolt <npkd@protonmail.com>
2023-06-20 16:02:37 +0300

    EXL-292 - Moved "Monthly plan" from iService2
    - Added minimal changes to original "Monthly plan" to make it work with iService3
    - Added "Monthly plan" menu item in "Views" menu
    - Added missing "old_name" values to printer_customfields.json

--- plugin/front/monthlyplan.php
+++ plugin/front/monthlyplan.php
@@ -3,7 +3,7 @@
 // Imported from iService2, needs refactoring. Original file: "planlunar.php".
 require "../inc/includes.php";
 
-//Session::checkRight("plugin_iservice_planlunar", READ); TODO: check this
+//Session::checkRight("plugin_iservice_planlunar", READ);
 
 Html::header(
     __("iService", "iservice"),
