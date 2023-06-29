b1aede70a41de72dd6413d3932ae2e6436c0e96e
codefactor-io <support@codefactor.io>
2023-06-29 10:10:48 +0000

    [CodeFactor] Apply fixes

--- plugin/front/admintask.php
+++ plugin/front/admintask.php
@@ -16,7 +16,7 @@ if ($task !== 'DataIntegrityTest') {
     Session::checkRight("plugin_iservice_admintask_$task", READ);
 }
 
-$task_class = "PluginIserviceTask_$task";
+$task_class  = "PluginIserviceTask_$task";
 $task_object = new $task_class();
 
 PluginIserviceHtml::header($task_object->getTitle(), filter_input(INPUT_SERVER, 'PHP_SELF'));
