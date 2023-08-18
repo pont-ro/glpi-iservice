4e31c90f3120254052ccc13be06dce0c5aa8fffd
Zsolt <npkd@protonmail.com>
2023-08-18 15:43:22 +0300

    EXL-300 Fixed paramater for getRowBackgroundClass in eval call

--- plugin/inc/specialviews/facturiclient.class.php
+++ plugin/inc/specialviews/facturiclient.class.php
@@ -220,7 +220,7 @@ class FacturiClient extends View
                         ",
             'default_limit' => 10,
             'show_limit' => Session::haveRight('plugin_iservice_view_facturi_client', UPDATE),
-            'row_class' => 'function:\GlpiPlugin\Iservice\Specialviews\FacturiClient::getRowBackgroundClass($row);',
+            'row_class' => 'function:\GlpiPlugin\Iservice\Specialviews\FacturiClient::getRowBackgroundClass($row_data);',
             'filters' => [
                 'nrfac' => [
                     'type' => 'text',
