f00d67c234aeb0eb481f68c28ff7254ea96f4208
Zsolt <npkd@protonmail.com>
2023-08-24 11:53:10 +0300

    EXL-300 Fixed kcsrft url param handling

--- plugin/inc/includes.php
+++ plugin/inc/includes.php
@@ -1,4 +1,9 @@
 <?php
+
+if (filter_input(INPUT_GET, 'kcsrft') || filter_input(INPUT_POST, 'kcsrft')) {
+    define('GLPI_KEEP_CSRF_TOKEN', true);
+}
+
 if (strpos(realpath(getcwd()), 'iService\plugin') > 0) {
     // This is a hack to make the plugin work in a development environment.
     global $CFG_GLPI;
