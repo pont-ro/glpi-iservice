86809cbd6c7df9adc603383b0f78348fea2ba666
zsso <78356157+zsso@users.noreply.github.com>
2023-09-13 12:34:50 +0300

    EXL-318 Changed hmarfa.export.default_path config (#79)
    - Removed clearCondition from cartridgeitem import config

--- plugin/config/import/cartridgeitem.php
+++ plugin/config/import/cartridgeitem.php
@@ -3,7 +3,6 @@ return [
     'itemTypeClass'   => CartridgeItem::class,
     'oldTable'        => 'glpi_cartridgeitems',
     'identifierField' => ['name', 'ref', 'is_deleted'],
-    'clearCondition'  => "is_deleted != 1",
     'foreignKeys'     => [
         'locations_id'          => 'Location',
         'cartridgeitemtypes_id' => 'CartridgeItemType',
