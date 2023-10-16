3d1a8fff2ce7f928dc509d1a1b083a65338a0758
Zsolt <npkd@protonmail.com>
2023-10-16 17:48:36 +0300

    EXL-339 - Changed helpdesk create ticket menu item link to navigate to inquiry form

--- plugin/inc/utils/redefinemenus.class.php
+++ plugin/inc/utils/redefinemenus.class.php
@@ -135,11 +135,12 @@ class RedefineMenus
         $menus['specialViews']['title'] = _n('Special View', 'Special Views', Session::getPluralNumber(), 'iservice');
         $menus['specialViews']['icon']  = 'ti ti-eye';
 
-        $menus['assets']['content']['cartridgeitem']['page'] = "$CFG_PLUGIN_ISERVICE[root_doc]/front/views.php?view=GlpiPlugin\Iservice\SpecialViews\Cartridges";
-        $menus['assets']['content']['printer']['page']       = "$CFG_PLUGIN_ISERVICE[root_doc]/front/views.php?view=GlpiPlugin\Iservice\SpecialViews\Printers";
-        $menus['management']['content']['contract']['page']  = "$CFG_PLUGIN_ISERVICE[root_doc]/front/views.php?view=GlpiPlugin\Iservice\SpecialViews\Contracts";
-        $menus['tools']['content']['reminder']['page']       = "$CFG_PLUGIN_ISERVICE[root_doc]/front/views.php?view=GlpiPlugin\Iservice\SpecialViews\Reminders";
-        $menus['helpdesk']['content']['ticket']['page']      = "$CFG_PLUGIN_ISERVICE[root_doc]/front/views.php?view=GlpiPlugin\Iservice\SpecialViews\Tickets";
+        $menus['assets']['content']['cartridgeitem']['page']     = "$CFG_PLUGIN_ISERVICE[root_doc]/front/views.php?view=GlpiPlugin\Iservice\SpecialViews\Cartridges";
+        $menus['assets']['content']['printer']['page']           = "$CFG_PLUGIN_ISERVICE[root_doc]/front/views.php?view=GlpiPlugin\Iservice\SpecialViews\Printers";
+        $menus['management']['content']['contract']['page']      = "$CFG_PLUGIN_ISERVICE[root_doc]/front/views.php?view=GlpiPlugin\Iservice\SpecialViews\Contracts";
+        $menus['tools']['content']['reminder']['page']           = "$CFG_PLUGIN_ISERVICE[root_doc]/front/views.php?view=GlpiPlugin\Iservice\SpecialViews\Reminders";
+        $menus['helpdesk']['content']['ticket']['page']          = "$CFG_PLUGIN_ISERVICE[root_doc]/front/views.php?view=GlpiPlugin\Iservice\SpecialViews\Tickets";
+        $menus['helpdesk']['content']['create_ticket']['page'] = "$CFG_PLUGIN_ISERVICE[root_doc]/front/ticket.form.php";
     }
 
 }
