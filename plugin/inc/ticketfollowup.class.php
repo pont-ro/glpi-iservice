e0a090fd2f0e6941381d48c71efd9cd8cb677f62
Zsolt <npkd@protonmail.com>
2023-10-27 10:55:11 +0300

    EXL-339 - Bug fix

--- plugin/inc/ticketfollowup.class.php
+++ plugin/inc/ticketfollowup.class.php
@@ -5,7 +5,7 @@ if (!defined('GLPI_ROOT')) {
     die("Sorry. You can't access directly to this file");
 }
 
-class PluginIserviceTicketFollowup extends ItilFollowup
+class PluginIserviceTicketFollowup extends ItIlFollowup
 {
 
     /**
