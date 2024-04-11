fbe68f1265cc3f69d7a5bfd4702c45a5cbf732ea
zsso <78356157+zsso@users.noreply.github.com>
2024-04-11 21:12:20 +0300

    EXL-366 - Added missing index on glpi_cartridgeitems (#118)
    - Added missing index on glpi_cartridgeitems
    - Removed caching from intorders
    - Fixed plugin version number
    - Fixed view columns sorting icon

--- plugin/config/config.php
+++ plugin/config/config.php
@@ -3,7 +3,6 @@
 /* Array keys must use dot notation, e.g. 'config_override.core.palette' => 'auror'. */
 
 return [
-    'version' => '0.0.4',
     'enabled_crons.h_marfa_import' => true,
     'enabled_crons.em_mailgate' => true,
     'enabled_crons.data_integrity_test' => false,
