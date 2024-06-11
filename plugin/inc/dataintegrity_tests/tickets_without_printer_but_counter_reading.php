d6054d4f8cb369af50f05409dc2947401946572c
Zsolt <npkd@protonmail.com>
2024-06-11 11:00:43 +0300

    EXL-289 - Removed unnecessary joins from tickets_without_printer_but_counter_reading.php
    - Adjusted time limit in tickets_without_printer_but_counter_reading test

--- plugin/inc/dataintegrity_tests/tickets_without_printer_but_counter_reading.php
+++ plugin/inc/dataintegrity_tests/tickets_without_printer_but_counter_reading.php
@@ -5,8 +5,6 @@ return [
         select t.id tid, t.total2_black_field, t.total2_color_field, REPLACE(SUBSTR(l.new_value, POSITION('(' IN l.new_value) + 1), ')', '') pid, p.name
         from glpi_plugin_iservice_tickets t
         left join glpi_items_tickets it on it.tickets_id = t.id and it.itemtype = 'Printer'
-        left join glpi_plugin_iservice_consumables_tickets ct on ct.tickets_id = t.id
-        left join glpi_plugin_iservice_cartridges_tickets crt on crt.tickets_id = t.id
         left join 
           (select max(id) max_lid, items_id tid
            from glpi_logs
@@ -17,7 +15,7 @@ return [
         where t.is_deleted = 0 and coalesce(t.total2_black_field, 0) + coalesce(t.total2_color_field, 0) > 0
           and it.id is null
           and (p.id is not null or l.new_value is null)
-          and t.effective_date_field > '2017-03-01 00:00:00'
+          and t.effective_date_field > '2024-06-15 00:00:00'
         group by t.id
         ",
     'test' => [
