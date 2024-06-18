9ca8198df49cb6088c58b5b41d9c7370de5263fa
zsso <78356157+zsso@users.noreply.github.com>
2024-06-18 13:34:34 +0300

    EXL-424 - Added detailed tooltip with uncloseable ticket reasons (#146)
    - Added detailed tooltip with uncloseable ticket reasons
    
    - Fixed tooltips on disabled buttons
    
    - Increased plugin version

--- plugin/setup.php
+++ plugin/setup.php
@@ -6,7 +6,7 @@ use GlpiPlugin\Iservice\Utils\ViewsMenu;
 use GlpiPlugin\Iservice\Utils\SpecialViewsMenu;
 use GlpiPlugin\Iservice\Utils\IserviceMenu;
 
-define('ISERVICE_VERSION', '3.0.0');
+define('ISERVICE_VERSION', '3.0.1');
 
 if (!defined("PLUGIN_ISERVICE_DIR")) {
     define("PLUGIN_ISERVICE_DIR", GLPI_ROOT . "/plugins/iservice");
