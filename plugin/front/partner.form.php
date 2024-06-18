719eaf8c003d4cccb39133b24cd686967c8e0b6d
Zsolt <npkd@protonmail.com>
2024-06-18 19:01:09 +0300

    EXL-415 - Fix

--- plugin/front/partner.form.php
+++ plugin/front/partner.form.php
@@ -6,27 +6,28 @@ use GlpiPlugin\Iservice\Utils\ToolBox as IserviceToolBox;
 
 $partner = new PluginIservicePartner();
 
-$id = IserviceToolBox::getInputVariable('id');
-$update = IserviceToolBox::getInputVariable('update');
-$partner_contacted = IserviceToolBox::getInputVariable('partner_contacted');
+$id                  = IserviceToolBox::getInputVariable('id');
+$update              = IserviceToolBox::getInputVariable('update');
+$partner_contacted   = IserviceToolBox::getInputVariable('partner_contacted');
 $generate_magic_link = IserviceToolBox::getInputVariable('generate_magic_link');
 
 $post = filter_input_array(INPUT_POST);
 
 if (!empty($generate_magic_link) && !empty($id)) {
-	$partner->check($id, UPDATE);
-	PluginIservicePartner::generateNewMagicLink($id);
-	Html::back();
+    $partner->check($id, UPDATE);
+    PluginIservicePartner::generateNewMagicLink($id);
+    Html::back();
 } elseif (!empty($update) && !empty($id)) {
-	$partner->check($id, UPDATE);
-	$partner->update($post);
-	if (!empty($post['_customfields'])) {
-		$partner_customfields = new PluginFieldsSuppliersuppliercustomfield();
+    $partner->check($id, UPDATE);
+    $partner->update($post);
+    if (!empty($post['_customfields'])) {
+        $partner_customfields = new PluginFieldsSuppliersuppliercustomfield();
         PluginIserviceDB::populateByItemsId($partner_customfields, $id);
-		$post['_customfields'][$partner_customfields->getIndexName()] = $partner_customfields->getID();
-		$partner_customfields->update($post['_customfields']);
-	}
-  Html::back();
+        $post['_customfields'][$partner_customfields->getIndexName()] = $partner_customfields->getID();
+        $partner_customfields->update($post['_customfields']);
+    }
+
+    Html::back();
 }
 
 Html::displayNotFoundError();
