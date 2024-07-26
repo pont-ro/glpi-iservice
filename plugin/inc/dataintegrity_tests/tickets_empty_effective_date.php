08ab680247a660be0ac3ab0148449897a03e80a7
Zsolt <npkd@protonmail.com>
2024-07-26 09:36:00 +0300

    EXL-460 -  Added date > data_integrity_tests_date_from to self test checking empty effective date tickets

--- plugin/inc/dataintegrity_tests/tickets_empty_effective_date.php
+++ plugin/inc/dataintegrity_tests/tickets_empty_effective_date.php
@@ -8,13 +8,14 @@ return [
             items_id tid
           , effective_date_field 
         from glpi_plugin_fields_ticketticketcustomfields
-        where effective_date_field = '' or effective_date_field is null
+        left join glpi_tickets t on t.id = items_id
+        where (effective_date_field = '' or effective_date_field is null) and t.date > '" . PluginIserviceConfig::getConfigValue('data_integrity_tests_date_from') . "'
         ",
     'test' => [
         'alert' => true,
         'type' => 'compare_query_count',
         'zero_result' => [
-            'summary_text' => "There are no tickets with empty effective date.",
+            'summary_text' => "There are no tickets with empty effective date",
         ],
         'positive_result' => [
             'summary_text' => "There are {count} tickets with empty effective date.",
