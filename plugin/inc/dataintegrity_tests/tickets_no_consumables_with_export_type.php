e65245076a2338a0d8eb727669afecfdd5d66190
hupu <hlavathy.peter@gmail.com>
2024-10-29 06:59:24 +0200

    EXL-493 fiexed tickets_no_consumables_with_export_type.php

--- plugin/inc/dataintegrity_tests/tickets_no_consumables_with_export_type.php
+++ plugin/inc/dataintegrity_tests/tickets_no_consumables_with_export_type.php
@@ -4,9 +4,10 @@ return [
     'query' => "
             select 
                 t.id tid
-              , GROUP_CONCAT(ct.plugin_iservice_consumables_id SEPARATOR ', ') consumables
               , t.effective_date_field
+              , ted.name export_type
             from glpi_plugin_iservice_tickets t
+            join glpi_plugin_fields_ticketexporttypedropdowns ted on ted.id = t.plugin_fields_ticketexporttypedropdowns_id
             left join glpi_plugin_iservice_consumables_tickets ct on ct.tickets_id = t.id
             where t.is_deleted = 0
               and t.status = 6 
@@ -18,11 +19,11 @@ return [
     'test' => [
         'type' => 'compare_query_count',
         'zero_result' => [
-            'summary_text' => 'There are no tickets with consumables without export type',
+            'summary_text' => 'There are no tickets with export type set, but no consumables',
         ],
         'positive_result' => [
-            'summary_text' => 'There are {count} tickets with consumables without export type',
-            'iteration_text' => "Ticket <a href='$CFG_PLUGIN_ISERVICE[root_doc]/front/ticket.form.php?id=[id]' target='_blank'>[id]</a> has [consumable_count] consumables but no export type",
+            'summary_text' => 'There are {count} tickets with export type set, but no consumables',
+            'iteration_text' => "Ticket <a href='$CFG_PLUGIN_ISERVICE[root_doc]/front/ticket.form.php?id=[id]' target='_blank'>[id]</a> with effective date <b>[effective_date_field]</b> has no consumables but <b>[export_type]</b> export type",
         ],
     ],
 ];
