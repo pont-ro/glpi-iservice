44aec81b727a45bd3000d673d80454d9ad4c34e2
hupu <hlavathy.peter@gmail.com>
2025-10-14 11:17:20 +0300

    EXL-548 Tooltip format fix for the Used consumables dropdown

--- plugin/ajax/getUsedConsumablesDropdownValuesForTicket.php
+++ plugin/ajax/getUsedConsumablesDropdownValuesForTicket.php
@@ -21,7 +21,7 @@ $consumables = PluginIserviceDB::getQueryResult("
     select 
         ct.plugin_iservice_consumables_id cid
       , c.name
-      , group_concat(distinct(case when it.items_id <> $printerId then concat('  - ', p.serial, ' [', pm.name, ']') end) separator '\n') printers
+      , group_concat(distinct(case when it.items_id <> $printerId then concat('  - ', p.serial, '(', pm.name, ')') end) separator '\n') printers
       , a.cod used
     from glpi_plugin_iservice_consumables c
     join glpi_plugin_iservice_consumables_tickets ct on ct.plugin_iservice_consumables_id = c.id
