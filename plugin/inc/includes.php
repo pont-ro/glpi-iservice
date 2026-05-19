3b0ddbea1c37dea3ba44cceb0ec3ef167a30c828
hupu <hlavathy.peter@gmail.com>
2026-05-19 04:47:55 +0300

    Small changes
    - add support for lando dev environment
    - updated the docs how to install Glpi properly in the local env

--- plugin/inc/includes.php
+++ plugin/inc/includes.php
@@ -4,7 +4,7 @@ if (($_GET['kcsrft'] ?? null) || ($_POST['kcsrft'] ?? null)) {
     define('GLPI_KEEP_CSRF_TOKEN', true);
 }
 
-if (strpos(realpath(getcwd()), 'iService\plugin') > 0) {
+if (strpos(realpath(getcwd()), 'iService\plugin') > 0 || strpos(realpath(getcwd()), 'app/plugin') > 0) {
     // This is a hack to make the plugin work in a development environment.
     global $CFG_GLPI;
     $CFG_GLPI['root_doc'] = '';
--- setup/README.md
+++ setup/README.md
@@ -1,7 +1,7 @@
 # Setup DEV
 
 1. Unpack `setup/glpi.tgz` to the root folder (the package already contains the `glpi` folder)
-2. Set your webroot to `glpi`
+2. Set your webroot to `./glpi/public`
 3. Install **Glpi** (make sure you access Glpi via https)
 4. Unpack the plugins you need (probably all of them) from `setup/plugins` to `glpi/plugins`
 5. Run `./setup/setup-dev`
